900083d3ac4e5b41b7aa3fa3c8ae86ce859fa9e6
Bymbaaaa <bymba0870@gmail.com>
2025-05-17 17:16:07 +0800

    Header ynzalsan, Rubik font tohiruulsan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { Footer }from './components/Footer/footer';
 import Header from './components/Header/header';
 
 function App() {
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,23 +5,25 @@ import "./header.css";
 
 export default function Header() {
   return (
-    <div className="header">
-      <img src={logo} alt="logo" className="logo"/>
-      <nav className="nav">
-       <div className="nav-link-left">
-          <strong className="nav-link">НҮҮР</strong>
-          <strong className="nav-link">RFID ТУХАЙ</strong>
-          <strong className="nav-link">СИСТЕМ</strong>
-          <strong className="nav-link">БҮТЭЭГДЭХҮҮН</strong>
-          <strong className="nav-link">ТООЦООЛУУР</strong>
-          <strong className="nav-link">ХОЛБОГДОХ</strong>
-       </div>
-       <div className="nav-link-right">
-         <strong className="nav-link">ҮНИЙН САНАЛ</strong>
-         <strong className="nav-link">MN/ENG</strong>
-       </div>
-      </nav>
-    </div>
+      <header className="header">
+        
+        <nav className="navContainer">
+          <img src={logo} alt="logo" className="logo" />
+          <div className="navLinks">
+            <li className="navItem">НҮҮР</li>
+            <li className="navItem">RFID ТУХАЙ</li>
+            <li className="navItem">СИСТЕМ</li>
+            <li className="navItem">БҮТЭЭГДЭХҮҮН</li>
+            <li className="navItem">ТООЦООЛУУР</li>
+            <li className="navItem">ХОЛБОГДОХ</li>
+            <li className="navItem">ҮНИЙН САНАЛ</li>
+            <li className="navItem">MN/EN</li>
+          </div>
+           <button className="mobileMenuButton" aria-label="Toggle menu">
+            <i className="menuIcon">☰</i>
+          </button>
+        </nav>
+      </header>
   );    
 }
                                                                                                                                                                                

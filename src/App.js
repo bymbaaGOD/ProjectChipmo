97063160876ea492fbc6e301a517c3709ac46298
21issagold <orgilbatbold6@gmail.com>
2025-05-18 17:20:08 +0800

    rfid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/header';
+import AboutRFID from './pages/AboutRFID/AboutRFID';
 
 function App() {
   return (
     <div>
       <Header/>
+      <AboutRFID/>
     </div>
   );
 }

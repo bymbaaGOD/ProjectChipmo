8e264910e2e1b42d94adc63608c48c7fe28d1d78
Bymbaaaa <bymba0870@gmail.com>
2025-05-21 17:42:51 +0800

    New branch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from "./pages/HomePage/homePage"
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/footer";
+import AboutRFID from "./pages/AboutRFID/AboutRFID";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/system" element={<Systempage/>}/>
+        <Route path="/aboutRFID" element={<AboutRFID/>}/>
       </Routes>
       <Footer/>
     </Router>
